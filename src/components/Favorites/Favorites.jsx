ba21b947f5ecf34d7e4097473b8752dbcd5479e7
Lily LaCosse <lilylacosse@gmail.com>
2023-04-06 11:07:50 -0500

    updated Favorite component - added access to favoriteList reducer, mapped data from favoriteList, now to make FavGif component
    '

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -3,10 +3,17 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 function Favorites() {
+    const dispatch = useDispatch()
     // useEffect for initial GET with dispatch inside to trigger Saga GET which will pass data to the reducer 
+    useEffect(() => { dispatch({ type: "GET_FAVORITES" }) })
     // useSelector to access data from store/reducer 
+    const favoriteList = useSelector((store) => { store.favoriteList })
+
     render(<><h3>Favorited GIFs</h3>
         {/* .map favorites */}
+        <ul>{favoriteList.map((favGif) => {
+            return <FavGif key={favGif.id} favGif={favGif} />
+        })}</ul>
     </>)
 }
 
