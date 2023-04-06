4c87d0dfe24e2107c820bbc8e8ee0f120ef9c80e
Lily LaCosse <lilylacosse@gmail.com>
2023-04-06 10:26:15 -0500

    updated Favorites component to reflect necessary pieces for initial rendering of favorites table to DOM, now to work on favorites reducers and sagas

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,5 +1,13 @@
-function Favorites () {
-    // jdkjasdkjsdafhjl
+import React, { useEffect } from "react";
+// do we need to specifically import useEffect when it is already part of React which we have imported all of? 
+import { useDispatch, useSelector } from "react-redux";
+
+function Favorites() {
+    // useEffect for initial GET with dispatch inside to trigger Saga GET which will pass data to the reducer 
+    // useSelector to access data from store/reducer 
+    render(<><h3>Favorited GIFs</h3>
+        {/* .map favorites */}
+    </>)
 }
 
 export default Favorites;
