777f403f7617e88bdabf7a8491c06cdfee5b7736
Lily LaCosse <lilylacosse@gmail.com>
2023-04-06 10:13:28 -0500

    add dummy data for favorites category to .sql file

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import { Provider } from 'react-redux';
-import logger from 'redux-logger';
-import { createSagaMiddleware } from 'redux-saga';
-import { takeEvery, put } from 'redux-saga/effects';
-import axios from 'axios'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { takeEvery, put } from "redux-saga/effects";
+import axios from "axios";
 
-function* mainSaga () {
-    // sagas here dude
+function* mainSaga() {
+  // sagas here dude
 }
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore (
-    combineReducers({
-        //reducers goes here
-    }),
-    applyMiddleware(sagaMiddleware, logger)
-)
+const store = createStore(
+  combineReducers({
+    //reducers goes here
+  }),
+  applyMiddleware(sagaMiddleware, logger)
+);
 
-sagaMiddleware.run(mainSaga)
+sagaMiddleware.run(mainSaga);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
