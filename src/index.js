7a6dc0bb78408a66555c27252e21fe3c8fd272bc
Lily LaCosse <lilylacosse@gmail.com>
2023-04-06 10:41:47 -0500

    set up reducer to hold favoriteList and saga to send HTTP get request to retrieve favorites db data and transfer the response to the reducer, now to work on accessing reducer state in Favorites component

--- src/index.js
+++ src/index.js
@@ -8,15 +8,42 @@ import createSagaMiddleware from "redux-saga";
 import { takeEvery, put } from "redux-saga/effects";
 import axios from "axios";
 
+//SAGA ROOT or WATCHER
 function* mainSaga() {
   // sagas here dude
+  yield takeEvery("GET_FAVORITES", getFavorites);
 }
+
+// SAGAS
+function* getFavorites() {
+  try {
+    // HTTP call to server to return favorite DB results
+    const newFavorites = yield axios.get("api/favorite");
+    // Saga put to turn server response data over to favoriteList reducer
+    yield put({ type: "SET_FAVORITES", payload: newFavorites.data });
+  } catch (err) {
+    // no errors please!
+    console.error(err);
+  }
+}
+
+//REDUCERS
+// reducer to hold favorite table data so we access in the Favorites component and render to the DOM
+const favoriteList = (state = [], action) => {
+  switch (action.type) {
+    case "SET_FAVORITES":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   combineReducers({
     //reducers goes here
+    favoriteList,
   }),
   applyMiddleware(sagaMiddleware, logger)
 );
